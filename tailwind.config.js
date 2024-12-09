f28f54883524a0fdcf9d928d35592227559b6d38
Elyes <elyes.h77@gmail.com>
2024-12-10 00:39:04 +0100

    dark mod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // Ajoutez cette ligne
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
